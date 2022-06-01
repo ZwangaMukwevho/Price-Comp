6060547fa9e796042b815b257418b0ce25c6fc8e
ZwangaMukwevho <zwangatm3@gmail.com>
2022-06-01 23:07:45 +0200

    created provider for fetching scrapped products

---
+++ lib/providers/scrap_product.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ScrapProduct with ChangeNotifier {
+  final String id;
+  final String title;
+  final String promotion;
+  final double promotionPrice;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  ScrapProduct({
+    @required this.id,
+    @required this.title,
+    @required this.promotion,
+    @required this.promotionPrice,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  //Method for re-rolling value of favorite item when an error occured
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
+    // Change the is favorite variable
+    final originalValue = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+
+    // Link of url of product
+    final url =
+        'https://shoppingappflutter-9c12b.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
+
+    try {
+      final response = await http.put(
+        url,
+        body: json.encode(
+          isFavorite,
+        ),
+      );
+
+      // If there's an error when trying to update on the server
+      if (response.statusCode >= 400) {
+        _setFavValue(originalValue);
+      }
+    } catch (error) {
+      _setFavValue(originalValue);
+    }
+  }
+}
